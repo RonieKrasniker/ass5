a14cca3c21770027245dd9a51d94e29c8d0b2f57
Aharon Krasniker <aharon.krasniker@live.biu.ac.il>
2024-07-08 19:07:40 +0300

    new

--- src/Counter.java
+++ src/Counter.java
@@ -1,13 +1,46 @@
 /**
- * Counter interface.
+ * Counter Class.
  */
 public class Counter {
-    // add number to current count.
-    void increase(int number){
+    //fields
+    private int count;
 
+
+    //constructor
+    // create a new counter with a given count
+
+    /**
+     * Construct a counter.
+     *
+     * @param count the count of the counter
+     */
+    public Counter(int count) {
+        this.count = count;
     }
-    // subtract number from current count.
-    void decrease(int number);
+
+    // add number to current count.
+
+    /**
+     * Increase the count of blocks by number.
+     *
+     * @param number the number to increase the count by
+     */
+    void increase(int number) {
+        this.count += number;
+    }
+
+    // subtract number from current count of blocks
+    void decrease(int number) {
+        this.count -= number;
+    }
     // get current count.
-    int getValue();
+
+    /**
+     * Get the value of the counter.
+     *
+     * @return the value of the counter
+     */
+    int getValue() {
+        return this.count;
+    }
 }
