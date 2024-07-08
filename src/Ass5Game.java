b534c628036e0efe373e1c33c015587ea023a0d6
Aharon Krasniker <aharon.krasniker@live.biu.ac.il>
2024-07-08 19:43:17 +0300

    new

---
+++ src/Ass5Game.java
@@ -0,0 +1,15 @@
+/**
+ * The main method that run the game.
+ */
+public class Ass5Game {
+    /**
+     * The main method that run the game.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initialize();
+        game.run();
+    }
+}
